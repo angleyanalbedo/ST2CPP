e381699336b53e0002ee6617e009f14c71309675
angleyanalbedo <100198247+angleyanalbedo@users.noreply.github.com>
2026-06-30 23:30:44 +0800

    feat: 添加多个示例程序，测试类、方法调用及位运算功能

--- java/src/main/java/PLCTranslator/TranslateType/Class_decl/TranslateClass_decl.java
+++ java/src/main/java/PLCTranslator/TranslateType/Class_decl/TranslateClass_decl.java
@@ -1,8 +1,12 @@
 package PLCTranslator.TranslateType.Class_decl;
 
 import PLCSymbolAndScope.PLCSymbols.*;
+import PLCTranslator.GvlContext;
 import PLCTranslator.PLCTranslatorNew;
 import antlr4.PLCSTPARSERParser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TranslateClass_decl {
 
@@ -15,14 +19,25 @@ public class TranslateClass_decl {
         }
         // 翻译类中变量声明
         PLCClassDeclSymbol classDeclSymbol = (PLCClassDeclSymbol) PLCTranslatorNew.properties.get(ctx).get(0);
+        String className = classDeclSymbol.getName();
 
         // 类变为 C++ struct
-        sb.append("\nstruct " + classDeclSymbol.getName() + " {");
+        sb.append("\nstruct ").append(className).append(" {");
+        List<GvlContext.StructField> structFields = new ArrayList<>();
+        int currentOffset = 0;
         for (PLCVariable value : classDeclSymbol.getVariableMap().values()) {
             String nativeType = translatorNew.gvlCtx.toNativeType(value.getRuntimeTypeName());
-            sb.append("\n\t" + nativeType + " " + value.getName() + ";");
+            sb.append("\n\t").append(nativeType).append(" ").append(value.getName()).append(";");
+            int elemSize = translatorNew.gvlCtx.getTypeSize(nativeType);
+            int aligned = (currentOffset + elemSize - 1) / elemSize * elemSize;
+            structFields.add(new GvlContext.StructField(value.getName(), nativeType, aligned));
+            currentOffset = aligned + elemSize;
         }
         sb.append("\n};");
+
+        // 注册 CLASS 类型为 struct（与 FB 相同逻辑）
+        GvlContext.StructLayout layout = new GvlContext.StructLayout(className, structFields, currentOffset);
+        translatorNew.gvlCtx.registerStructType(className, layout);
 
         return sb.toString();
     }
